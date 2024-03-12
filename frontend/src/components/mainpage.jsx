0ecb0c3776a59d5bc89ce6a015765fb2b47ae21d
nishantjo-c <nishantjoshiofficial@gmail.com>
2024-03-12 17:58:17 +0530

    first commit

---
+++ frontend/src/components/mainpage.jsx
@@ -0,0 +1,21 @@
+function Main() {
+  return (
+    <>
+      <header>
+        <div style={{ display: "inline", width: "20px", height: "10px" }}>
+          -
+        </div>
+      </header>
+      <body>
+        <main>
+          <h1>Danu & Architect</h1>
+        </main>
+      </body>
+      <footer>
+        <h3>We don’t just make your house, we make your dream come true</h3>
+      </footer>
+    </>
+  );
+}
+
+export default Main;
